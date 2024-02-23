2fe0accfc0b66cea8c2557ceb67c870b1fbe58a3
raviehasan <ravie.hasan@ui.ac.id>
2024-02-23 13:25:21 +0700

    implements SRP & DIP (remove public modifier on services interface)

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,13 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-
-    public void delete(String productId);
-
-    public List<Product> findAll();
-
-    public Product getProductById(String productId);
-
-    public void edit(String productId, Product editedProduct);
+    Product create(Product product);
+    void delete(String productId);
+    List<Product> findAll();
+    Product getProductById(String productId);
+    void edit(String productId, Product editedProduct);
 }
