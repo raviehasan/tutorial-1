0220f11fe7358b339a9d933087c4323610ccee62
raviehasan <ravie.hasan@ui.ac.id>
2024-02-23 13:52:55 +0700

    removed unnecessary comment and variable

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -9,7 +9,6 @@ import java.util.UUID;
 
 @Repository
 public class CarRepository {
-    static int id = 0;
     private List<Car> carData = new ArrayList<>();
 
     public Car create(Car car) {
@@ -38,15 +37,16 @@ public class CarRepository {
         for (int i = 0; i < carData.size(); i++) {
             Car car = carData.get(i);
             if (car.getCarId().equals(id)) {
-                // Update the existing car with the new information
                 car.setCarName(updatedCar.getCarName());
                 car.setCarColor(updatedCar.getCarColor());
                 car.setCarQuantity(updatedCar.getCarQuantity());
                 return car;
             }
         }
-        return null; // Handle the case where the car is not found
+        return null;
     }
 
-    public void delete(String id) { carData.removeIf(car -> car.getCarId().equals(id)); }
+    public void delete(String id) {
+        carData.removeIf(car -> car.getCarId().equals(id));
+    }
 }
