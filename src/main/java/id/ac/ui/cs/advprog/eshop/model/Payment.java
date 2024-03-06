0740ff78f8fbf2ceccaacda19d038232109d9cd4
raviehasan <ravie.hasan@ui.ac.id>
2024-03-06 20:42:44 +0700

    [GREEN] implement voucher payment

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -50,3 +50,47 @@ public class Payment {
         this.status = status;
     }
 }
+
+@Getter
+class VoucherPayment extends Payment {
+    public VoucherPayment(String id, String method, Order order, Map<String, String> paymentData) {
+        super(id, method, order, paymentData);
+    }
+
+    public VoucherPayment(String id, String method, Order order, Map<String, String> paymentData, String status) {
+        super(id, method, order, paymentData, status);
+    }
+
+    @Override
+    protected void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData.isEmpty())
+            throw new IllegalArgumentException("Payment data must not be empty.");
+
+        if (paymentData.get("voucherCode").isEmpty())
+            throw new IllegalArgumentException("Voucher code must not be empty.");
+
+        if (!this.checkVoucherCodeValidity(paymentData.get("voucherCode")))
+            throw new IllegalArgumentException("Voucher code is invalid.");
+
+        this.paymentData = paymentData;
+    }
+
+    private boolean checkVoucherCodeValidity(String voucherCode) {
+        if (voucherCode.length() != 16)
+            return false;
+
+        if (!voucherCode.startsWith("ESHOP"))
+            return false;
+
+        return this.hasExactly8NumericalCharacters(voucherCode);
+    }
+
+    private boolean hasExactly8NumericalCharacters(String voucherCode) {
+        long numericalCharactersCount = 0;
+        for (char chari : voucherCode.toCharArray()) {
+            if (Character.isDigit(chari))
+                numericalCharactersCount++;
+        }
+        return numericalCharactersCount == 8;
+    }
+}
